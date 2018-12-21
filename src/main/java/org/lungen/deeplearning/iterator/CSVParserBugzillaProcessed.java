de0499d1282b524b020727c5ec3bb1414ba0783a
Gennady Lungu <glungu@amdocs.com>
2018-12-21 16:13:30 +0300

    write dictionary files during processing

--- src/main/java/org/lungen/deeplearning/iterator/CSVParserBugzillaProcessed.java
+++ src/main/java/org/lungen/deeplearning/iterator/CSVParserBugzillaProcessed.java
@@ -1,14 +1,16 @@
 package org.lungen.deeplearning.iterator;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-
 import java.io.File;
-import java.text.ParseException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 /**
  * CSVParserBugzillaProcessed
@@ -18,24 +20,44 @@ import java.util.Map;
 public class CSVParserBugzillaProcessed extends CSVParser {
 
     private static final SimpleDateFormat DF = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+    private final File workDir;
 
     public CSVParserBugzillaProcessed(File csvFile, boolean hasHeaders) {
         super(csvFile, hasHeaders);
+        this.workDir = csvFile.getParentFile();
     }
 
     public void process() {
         String[] columnNames = {"status", "resolution", "severity", "priority", "creator", "assigned_to", "component"};
         Map<String, Map<String, Integer>> categoricalMap = categoricalStringToNumeric(columnNames);
-        // output categorical values mapping
-        categoricalMap.keySet().forEach(name -> {
-            Map<String, Integer> valuesMap = categoricalMap.get(name);
-            System.out.println("Categorical feature '" + name + "': ");
-            valuesMap.keySet().forEach(i -> System.out.println(i + " -> " + valuesMap.get(i)));
-        });
+
+        // process lines
         parsedLines.forEach(parsedLine -> {
             categoricalStringToNumeric(parsedLine, categoricalMap, columnNames);
             dateToNumeric(parsedLine, "creation_time", "resolution_time");
             arrayToFirstValue(parsedLine, "blocks", "depends_on");
+        });
+
+        // categorical values mapping to dictionary files
+        categoricalMap.keySet().forEach(name -> {
+            Map<String, Integer> valuesMap = categoricalMap.get(name);
+            File dictFile = new File(workDir, "dict-" + name + ".txt");
+            int MAXLEN = valuesMap.keySet().stream().mapToInt(String::length).max().orElse(0) + 1;
+
+            try (FileOutputStream dictFileStream = new FileOutputStream(dictFile)) {
+                valuesMap.entrySet().stream().sorted(Map.Entry.comparingByValue()).forEach(entry -> {
+                    String key = entry.getKey();
+                    try {
+                        StringBuilder indent = new StringBuilder();
+                        IntStream.range(0, MAXLEN - key.length()).forEach(n -> indent.append(" "));
+                        dictFileStream.write((key + ": " + indent.toString() + entry.getValue() + "\n").getBytes());
+                    } catch (IOException e) {
+                        log.error("Cannot write to file: " + dictFile, e);
+                    }
+                });
+            } catch (Exception e) {
+                log.error("Cannot open file: " + dictFile, e);
+            }
         });
     }
 
@@ -47,12 +69,8 @@ public class CSVParserBugzillaProcessed extends CSVParser {
                 JSONArray array = new JSONArray(unQuote(value));
                 String newValue = array.isEmpty() ? "" : array.get(0) + "";
                 parsedLine.set(index, newValue);
-
-                if (array.length() > 1) {
-                    System.out.println("Array more than one element: " + parsedLine.get(headers.get("id")));
-                }
             } catch (JSONException e) {
-                e.printStackTrace();
+                log.error("Error parsing JSON", e);
             }
         });
     }
@@ -72,27 +90,33 @@ public class CSVParserBugzillaProcessed extends CSVParser {
     private void dateToNumeric(List<String> parsedLine, String... columnNames) {
         Arrays.stream(columnNames).forEach(columnName -> {
             Integer ind = headers.get(columnName);
-            try {
+            if (parsedLine.size() > ind) {
                 String dateString = parsedLine.get(ind);
-                String numericString = "";
-                if (!dateString.equals("[]")) {
-                    numericString = DF.parse(dateString).getTime() + "";
+                try {
+                    String numericString = "";
+                    if (!dateString.equals("[]")) {
+                        numericString = DF.parse(dateString).getTime() + "";
+                    }
+                    parsedLine.set(ind, numericString);
+                } catch (Exception e) {
+                    log.error("Cannot parse date, id: " + parsedLine.get(headers.get("id")) + ", "
+                            + "index: " + ind + ", value: " + dateString);
                 }
-                parsedLine.set(ind, numericString);
-            } catch (Exception e) {
-                log.error("Cannot parse date: " + ind);
+            } else {
+                log.error("Cannot parse date, id: " + parsedLine.get(headers.get("id")) + ", "
+                        + "no such index: " + ind + ", size: " + parsedLine.size());
             }
         });
     }
 
     public static void main(String[] args) {
-        String dir = "C:/DATA/Projects/DataSets/Jnetx_Bugzilla";
-        File sourceFile = new File(dir, "bugzilla-jnetx-processed.csv");
+        String dir = "C:/Work/ML/bugzilla";
+        File sourceFile = new File(dir, "bugzilla-processed.csv");
 
         CSVParserBugzillaProcessed parser = new CSVParserBugzillaProcessed(sourceFile, true);
         parser.process();
 
-        File processedFile = new File(dir, "bugzilla-jnetx-processed-final.csv");
+        File processedFile = new File(dir, "bugzilla-processed-final.csv");
         parser.save(processedFile);
     }
 }
